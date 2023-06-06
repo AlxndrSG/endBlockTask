17081dd362dc9b59eb389594d6414b8e8c95ce05
Alexander <firefighter-911@rambler.ru>
2023-06-06 21:16:44 +0300

    Решение итоговой задачи

---
+++ endBlockTask/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.Clear();
+
+string[] arrayOne = new string[3] { "Hello", "2", ":-)" };
+//string[] arrayOne = new string[4] { "1234", "1567", "-2", "computer science" };
+//string[] arrayOne = new string[3] { "Russia", "Denmark", "Kazan" };
+
+string[] arrayTwo = new string[arrayOne.Length];
+
+NewSecondArray(arrayOne, arrayTwo);
+
+PrintArray(arrayOne);
+Console.Write(" -> ");
+PrintArray(arrayTwo);
+
+void NewSecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+
