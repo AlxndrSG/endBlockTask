a279c38a449769bb63045c980b894d1c785aeddf
Alexander <firefighter-911@rambler.ru>
2023-06-07 21:54:39 +0300

    Решение итоговой задачи с учетом замечаний преподавателя

---
+++ newEndBlockTask/Program.cs
@@ -0,0 +1,63 @@
+﻿
+Console.Clear();
+
+int size = Prompt("Введите длинну массива: ", "Ошибка! Введены некорректные данные!");
+
+string[] arrayOne = new string[size];
+Console.WriteLine($"Введите элементы массива через 'Enter': ");
+
+FillArray(arrayOne);
+
+int n = Prompt("Введите количество символов размера элемента массива: ", "Ошибка! Введены некорректные данные!");
+int secondSize = FindSecondSize(arrayOne);
+
+string[] arrayTwo = TransferElementFromArrayOne(arrayOne);
+Console.WriteLine($"[{String.Join(", ", arrayOne)}] -> [{String.Join(", ", arrayTwo)}]");
+
+void FillArray(string[] arr)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Console.ReadLine()!;
+    }
+}
+
+string[] TransferElementFromArrayOne(string[] arr)
+{
+    string[] arrayTwo = new string[secondSize];
+    for (int i = 0, j = 0; i < size; i++)
+    {
+        if (arr[i].Length <= n)
+        {
+            arrayTwo[j] = arr[i];
+            j++;
+        }
+    }
+    return arrayTwo;
+}
+
+int FindSecondSize(string[] arr)
+{
+    int sizeSecond = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i].Length <= n)
+            sizeSecond++;
+    }
+    return sizeSecond;
+}
+
+int Prompt(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userInput))
+        {
+            return userInput;
+        }
+        else Console.WriteLine(errorMessage);
+    }
+}
+
+
